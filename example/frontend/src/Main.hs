e8c4f020f7556aeb24764b5bf9c62171113ed7da
Dave Laing <dave.laing.80@gmail.com>
2017-08-22 10:15:17 +1000

    Cleans up the frontend

--- example/frontend/src/Main.hs
+++ example/frontend/src/Main.hs
@@ -11,43 +11,48 @@ module Main (
     main
   ) where
 
-import qualified Data.Text as T
-import qualified Data.Map as M
+import Control.Lens (view)
+
+import Data.Text (Text)
+import qualified Data.Text as Text
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Reflex.Dom
 
 import CounterWidget
 
-counterWithClose :: MonadWidget t m 
-                 => T.Text 
-                 -> T.Text 
-                 -> m (Event t ())
+counterWithClose :: MonadWidget t m
+                 => Text
+                 -> Text
+                 -> EventWriterT t (Set Text) m ()
 counterWithClose url index = el "div" $ mdo
   text index
-  counterWidget (T.append url index) eClose
+  counterWidget (Text.append url index) eClose
   eClose <- button "Close"
-  return eClose
+  tellEvent $ Set.singleton index <$ eClose
 
 counterListWidget :: MonadWidget t m
-                  => T.Text 
+                  => Text
                   -> m ()
 counterListWidget url = mdo
 
   tiIndex <- textInput def
   eAddClick <- button "Add"
 
-  let 
-    eInsert = 
-      (\k -> M.singleton k (Just ())) <$> current (_textInput_value tiIndex) <@ eAddClick
-    eDeletes = 
-      switch .
-      fmap (fmap (Nothing <$) . mergeMap) .
-      current $ 
-      dMap
-    eMap = 
-      leftmost [eInsert, eDeletes]
+  dModel <- foldDyn ($) Map.empty .
+            mergeWith (.) $ [
+              Map.insert <$> current (view textInput_value tiIndex) <@> eAddClick
+            , flip (foldr Map.delete) <$> eRemoves
+            ]
 
-  dMap <- listHoldWithKey M.empty eMap (\k _ -> counterWithClose url k)
+  (_, eRemoves) <- runEventWriterT .
+                   listWithKey dModel $ \k _ ->
+                     counterWithClose url k
 
   return ()
 
@@ -55,11 +60,11 @@ main :: IO ()
 main = mainWidget $ el "div" $ do
   el "div" $
     counterWidget "counter/unique" never
-  el "div" $ 
+  el "div" $
     counterWidget "counter/shared" never
-  el "div" $ 
+  el "div" $
     counterWidget "counter/shareddb" never
   el "div" $
     counterWidget "counter/bounded" never
-  el "div" $ 
-    counterListWidget "counter/indexed/" 
+  el "div" $
+    counterListWidget "counter/indexed/"
