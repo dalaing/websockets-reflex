7ca6124773d6131838ff086f02bccabe04c2a322
Dave Laing <dave.laing.80@gmail.com>
2017-10-20 09:54:19 +1000

    Another front-end change

--- example/frontend/src/CounterWidget.hs
+++ example/frontend/src/CounterWidget.hs
@@ -6,6 +6,7 @@ Stability   : experimental
 Portability : non-portable
 -}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE RecursiveDo #-}
 module CounterWidget (
     counterWidget
@@ -25,52 +26,61 @@ import qualified Data.Text as T
 
 import Commands
 
-activeCounterWidget :: MonadWidget t m 
-                    => Event t Int
-                    -> Bool
-                    -> m (Event t Int, Event t ())
-activeCounterWidget _ False = do
+activeCounterWidgetClosed ::
+  MonadWidget t m =>
+  Event t Int ->
+  m (Event t Int, Event t ())
+activeCounterWidgetClosed _ = do
   text "Waiting for websocket..."
   return (never, never)
-activeCounterWidget eTotal True = do
+
+activeCounterWidgetOpen ::
+  MonadWidget t m =>
+  Event t Int ->
+  m (Event t Int, Event t ())
+activeCounterWidgetOpen eTotal = do
   tiAdd <- textInput def
   eAddClick <- button "Add"
-
   eClear <- button "Clear"
 
   tiTotal <- textInput $
     def & textInputConfig_setValue .~ fmap (T.pack . show) eTotal
 
-  let 
+  let
     eAdd =
       fmapMaybe (readMaybe . T.unpack) (current (tiAdd ^. textInput_value) <@ eAddClick)
 
   return (eAdd, eClear)
 
-counterWidget :: MonadWidget t m 
+counterWidget :: MonadWidget t m
               => T.Text
               -> Event t ()
               -> m ()
 counterWidget url eClose = mdo
-  ee <- dyn (activeCounterWidget eTotal <$> dOpen)
-  eAdd <- switchPromptly never (fst <$> ee)
-  eClear <- switchPromptly never (snd <$> ee)
+  let
+    acwClosed eTotal = Workflow $ do
+      x <- activeCounterWidgetClosed eTotal
+      pure (x, acwOpen eTotal   <$ ws ^. webSocket_open)
+
+    acwOpen eTotal = Workflow $ do
+      x <- activeCounterWidgetOpen eTotal
+      pure (x, acwClosed eTotal <$ ws ^. webSocket_close)
+
+  dpe <- workflow $ acwClosed eTotal
+
+  let
+    eAdd   = switch . current . fmap fst $ dpe
+    eClear = switch . current . fmap snd $ dpe
 
   let wsReq = leftmost [
-                AddReq <$> eAdd
-              , ClearReq <$ eClear
+                AddReq   <$> eAdd
+              , ClearReq <$  eClear
               ]
 
   let fullUrl = T.append "ws://127.0.0.1:8000/" url
   ws <- webSocket fullUrl $
-    def & webSocketConfig_send .~ fmap (pure . toStrict . encode) wsReq
+    def & webSocketConfig_send  .~ fmap (pure . toStrict . encode) wsReq
         & webSocketConfig_close .~ fmap (const (1000, "bye")) eClose
-
-  dOpen <- holdDyn False . 
-           leftmost $ [
-             True <$ ws ^. webSocket_open
-           , False <$ ws ^. webSocket_close
-           ]
 
   let
     wsRes =
